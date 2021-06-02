939cd9e05f2f3e86fea7e2c85bfc42a14011f64a
Alonso Valdivia <comofuncionaelmundoporalonso@gmail.com>
2021-06-02 18:19:56 -0500

    agregadas otras bases de datos: OuluCasia y MUG

---
+++ other-datasets/OuluCasia.py
@@ -0,0 +1,43 @@
+from PIL import Image
+from matplotlib import cm
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import torch, os
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import Dataset, DataLoader, random_split
+from torchvision import transforms, utils
+from torchvision.transforms import ToTensor
+from torchvision.utils import make_grid
+import csv
+
+class OuluCasia(Dataset):
+    
+    def __init__(self, data_path, transforms=None):
+        images,labels = self.getSet(data_path)
+        self.X = images
+        self.y = labels
+        self.transforms = transforms
+        
+    def __len__(self):
+        return len(self.X)
+    
+    def __getitem__(self, i):
+        data = Image.open(self.X[i])
+        if self.transforms is not None:
+            data = self.transforms(data)
+        label = int(self.y[i])
+        return (data, label)
+
+    def getSet(self, data_path):
+        file_path = os.path.join(data_path)
+        file = open(file_path,'r')
+        lines = file.readlines()
+        images = []
+        labels = []
+        for line in lines:
+            tokens = line.split(' ')
+            images.append(tokens[0])
+            labels.append(tokens[1][0])
+        return images,labels
---
+++ other-datasets/preprocess_OuluCasia.py
@@ -0,0 +1,62 @@
+
+# labels 0=anger 1=disgust, 2=fear, 3=happy, 4=sadness, 5=surprise, 6=neutral
+
+
+import csv
+import os
+import numpy as np
+import random
+from config import OuluCasia_path,training_ratio
+
+anger_path = os.path.join(OuluCasia_path, 'anger')
+disgust_path = os.path.join(OuluCasia_path, 'disgust')
+fear_path = os.path.join(OuluCasia_path, 'fear')
+happy_path = os.path.join(OuluCasia_path, 'happiness')
+sadness_path = os.path.join(OuluCasia_path, 'sadness')
+surprise_path = os.path.join(OuluCasia_path, 'surprise')
+neutral_path = os.path.join(OuluCasia_path, 'neutral')
+
+
+
+foldspath = os.path.join(OuluCasia_path,'Folds')
+if not os.path.exists(foldspath):
+    print("fold creado")
+    os.makedirs(foldspath)
+
+train_data = []
+test_data = []
+
+def extract_emotion(emotion_path,id):
+    global train_file,test_file,training_ratio
+    files = os.listdir(emotion_path) #Chambeando con los angry
+    tam = len(files)
+    train_len = int(tam*training_ratio)
+    i=0
+    while i < train_len:
+        train_data.append(os.path.join(emotion_path,files[i])+' '+str(id)+'\n')
+        i+=1
+    while i <len(files):
+        test_data.append(os.path.join(emotion_path,files[i])+' '+str(id)+'\n')
+        i+=1
+
+def list_to_file(lista,path_file):
+    file = open(path_file,"w")
+    for line in lista:
+        file.write(line)
+    file.close()
+
+extract_emotion(anger_path,0)
+extract_emotion(disgust_path,1)
+extract_emotion(fear_path,2)
+extract_emotion(happy_path,3)
+extract_emotion(sadness_path,4)
+extract_emotion(surprise_path,5)
+extract_emotion(neutral_path,6)
+
+random.seed(42)
+random.shuffle(train_data)
+random.shuffle(test_data)
+
+list_to_file(train_data,os.path.join(foldspath,'train_file.txt'))
+list_to_file(test_data,os.path.join(foldspath,'test_file.txt'))
+
